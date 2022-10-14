35933b6dc224451862fd427b27e1a570e60ad145
flavono123 <flavono123@gmail.com>
2022-10-14 16:14:37 +0900

    script: Remove output file extentions, json
    Update the docker image(overwrite to 0.1.0):
    https://hub.docker.com/layers/vonogoru123/list-outdated-access-keys/0.1.0/images/sha256-4b7fb747603b41ea2ea4949395dc5b2404727cce5860026eb4d6c981e57d739e?context=repo
    
    Signed-off-by: flavono123 <flavono123@gmail.com>

--- list-outdated-access-keys.sh
+++ list-outdated-access-keys.sh
@@ -19,15 +19,15 @@ results_dir="results/$retention_ts"
 mkdir -p "$results_dir"
 
 echo "Create files with access keys created before ${retention_hours} hours ago(at ${retention_ts})"
-# Create the access key list json for each users
+# Create the access key list for each user
 aws iam list-users | \
   jq .Users[].UserName | \
   xargs -I{} sh -c \
     "aws iam list-access-keys --user-name {} | \
       jq -r '.AccessKeyMetadata |
         select(length != 0)[] | select(.CreateDate | fromdateiso8601 < $RETENTION_EPOCH) |
-        \"\(.CreateDate) - \(.AccessKeyId)\"' > $results_dir/{}.json && \
-      echo \"'$results_dir/{}.json' is created\""
+        \"\(.CreateDate) - \(.AccessKeyId)\"' > $results_dir/{} && \
+      echo \"'$results_dir/{}' is created\""
 
 
 # Delete empty files
